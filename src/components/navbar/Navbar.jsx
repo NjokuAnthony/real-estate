bb20eb3d7dfe9eaae2855c2a2e9cf34d11d5c23b
NjokuAnthony <a_njokuchibuzor@yahoo.com>
2023-07-22 17:19:55 +0100

    real-estate app

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {HiOutlineMenuAlt4} from 'react-icons/hi'
+import {FaRegTimesCircle} from 'react-icons/fa'
+import {BsFillHouseFill} from 'react-icons/bs'
+
+
+import './navbar.css'
+
+const Navbar = () => {
+
+
+
+  return (
+    <div className='navbar'>
+        <div className="container">
+            <h1><span><BsFillHouseFill />Real</span>Estate</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
