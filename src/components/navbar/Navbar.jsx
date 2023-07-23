b19fda3b925ae6a743ec2b8ed7d19ba89d119b9b
NjokuAnthony <a_njokuchibuzor@yahoo.com>
2023-07-23 17:55:27 +0100

    added function to the hambuger-menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import {HiOutlineMenuAlt4} from 'react-icons/hi'
 import {FaRegTimesCircle} from 'react-icons/fa'
 import {BsFillHouseFill} from 'react-icons/bs'
@@ -7,6 +8,10 @@ import {BsFillHouseFill} from 'react-icons/bs'
 import './navbar.css'
 
 const Navbar = () => {
+    const[click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click)
+
 
 
 
@@ -14,6 +19,15 @@ const Navbar = () => {
     <div className='navbar'>
         <div className="container">
             <h1><span><BsFillHouseFill />Real</span>Estate</h1>
+            <button className='btn'>Sign In</button>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                <li><a href='#'>Home</a></li>
+                <li><a href='#'>Search</a></li>
+                <li><a href='#'>About</a></li>
+                <li><a href='#'>Contact</a></li>
+            </ul>
+
+            <div className='hamburger'><HiOutlineMenuAlt4 onClick={handleClick} className='icon'/></div>
         </div>
     </div>
   )
