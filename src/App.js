b45e83fecf3329b3ba5002e2f725fa0fa5e431a9
NjokuAnthony <a_njokuchibuzor@yahoo.com>
2023-09-06 00:38:26 +0100

    Houses component add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './components/navbar/Navbar';
 import Hero from './components/hero/Hero';
+import Houses from './components/Houses/Houses';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Houses />
     </div>
   );
 }
